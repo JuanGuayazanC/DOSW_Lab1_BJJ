94fc3f08d54547ca6b1b400a0d74337ad1a453a5
BRAYAN LOAIZA LEAL <alvarezdairo9@gmail.com>
2026-01-26 17:26:21 -0500

    mision: repetir mensaje 3 veces

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,1 +1,12 @@
- 
+import java.util.stream.IntStream;
+
+public class Reto3 {
+
+    public static String mensaje(String cadena) {
+        StringBuilder sb = new StringBuilder();
+
+        IntStream.range(0, 3)
+                .forEach(i -> sb.append(cadena).append(" "));
+        return sb.toString().trim();
+    }
+}
