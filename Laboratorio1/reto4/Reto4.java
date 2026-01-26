cd9ef24d3bd3576fb67a4a454313ccc8b96b2a81
Juan Sebastián Guayazán Clavijo <juan.guayazan-c@mail.escuelaing.edu.co>
2026-01-26 18:37:27 -0500

    Implementar el método guardarEnHashMap
    método que reciba una lista de pares clave-valor (String, Integer) y los almacene en un HashMap, ignorando claves duplicadas (conserva el primer valor encontrado).

--- Laboratorio1/reto4/Reto4.java
+++ Laboratorio1/reto4/Reto4.java
@@ -1,1 +1,17 @@
+public static HashMap<String, Integer> guardarEnHashMap(
+        List<Map.Entry<String, Integer>> lista) {
+
+    HashMap<String, Integer> mapa = new HashMap<>();
+
+    for (int i = 0; i < lista.size(); i++) {
+        String clave = lista.get(i).getKey();
+        Integer valor = lista.get(i).getValue();
+
+        if (!mapa.containsKey(clave)) {
+            mapa.put(clave, valor);
+        }
+    }
+
+    return mapa;
+}
  
