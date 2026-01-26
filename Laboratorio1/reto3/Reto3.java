ba1c2e7e6d820f535002b0a714c8c5df87fb6479
Juan Sebastián Guayazán Clavijo <juan.guayazan-c@mail.escuelaing.edu.co>
2026-01-26 17:27:00 -0500

    Mision: Implementar el método de cadena inversa

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,1 +1,7 @@
- 
+public class Reto3 {
+
+    public static String mensaje(String cadena) {
+        StringBuffer men = new StringBuffer(cadena);
+        return men.reverse().toString();
+    }
+} 
