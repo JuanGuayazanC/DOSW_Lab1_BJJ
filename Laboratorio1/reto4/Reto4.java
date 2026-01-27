0d2cf041551b31dd27eb165de0ffd7771a207a3f
Juan Sebastián Guayazán Clavijo <juan.guayazan-c@mail.escuelaing.edu.co>
2026-01-26 19:05:50 -0500

    Refactorizar el método guardarEnHashMap en Reto4.java
    Añadir la clase y las librerias

--- Laboratorio1/reto4/Reto4.java
+++ Laboratorio1/reto4/Reto4.java
@@ -1,17 +1,24 @@
-public static HashMap<String, Integer> guardarEnHashMap(
-        List<Map.Entry<String, Integer>> lista) {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    HashMap<String, Integer> mapa = new HashMap<>();
+public class Reto4 {
 
-    for (int i = 0; i < lista.size(); i++) {
-        String clave = lista.get(i).getKey();
-        Integer valor = lista.get(i).getValue();
+    public static HashMap<String, Integer> guardarEnHashMap(
+            List<Map.Entry<String, Integer>> lista) {
 
-        if (!mapa.containsKey(clave)) {
-            mapa.put(clave, valor);
+        HashMap<String, Integer> mapa = new HashMap<>();
+
+        for (int i = 0; i < lista.size(); i++) {
+            String clave = lista.get(i).getKey();
+            Integer valor = lista.get(i).getValue();
+
+            if (!mapa.containsKey(clave)) {
+                mapa.put(clave, valor);
+            }
         }
-    }
 
-    return mapa;
+        return mapa;
+    }
 }
- 
+
