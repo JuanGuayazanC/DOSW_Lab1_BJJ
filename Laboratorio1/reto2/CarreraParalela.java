829cc5caad048897d540059f0359da243edd86ea
Juan Esteban Cruz <juanestebancruzrico@gmail.com>
2026-01-27 16:38:13 -0500

    Creacion con la funcion labda del maximo valor

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,1 +1,20 @@
+import java.util.List;
+
+import java.util.Optional;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = List.of(4, 9, 2, 15, 7);
+
+        Optional<Integer> maximo = numeros.stream()
+
+                .reduce((a, b) -> a > b ? a : b);
+
+        maximo.ifPresent(m -> System.out.println("El numero mayor es: " + m));
+
+    }
+
+}
  
