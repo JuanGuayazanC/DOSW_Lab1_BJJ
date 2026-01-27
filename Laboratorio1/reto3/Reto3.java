e1860f1329a3ddda128e1958673590ddf9557497
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-01-26 20:03:21 -0500

    cambios?

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,14 +1,13 @@
 public class Reto3 {
 
     public static String mensaje(String cadena) {
-        StringBuffer men = new StringBuffer(cadena);
+        StringBuffer men = new StringBuffer(mensaje);
         return men.reverse().toString();
     }
-
     public static void main(String[] args) {
 
-        String mensaje = System.out.readLine();
-        String resultado = Reto3.mensaje(mensaje);
+        String mensaje = "Hola mundo";
+        String resultado = EcoMisterioso.eco(mensaje);
 
         System.out.println(resultado);
     }
