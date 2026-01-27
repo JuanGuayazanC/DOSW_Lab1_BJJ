07da2defbdba2453e03c96b2f9f1afecafae3f06
Juan Esteban Cruz <juanestebancruzrico@gmail.com>
2026-01-27 17:41:11 -0500

    Añade funcion de verficacion si el numero es divisor de 2

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,20 +1,83 @@
 import java.util.List;
 
-import java.util.Optional;
-
 public class Main {
 
     public static void main(String[] args) {
 
         List<Integer> numeros = List.of(4, 9, 2, 15, 7);
 
-        Optional<Integer> maximo = numeros.stream()
+        ResultadoEstadistico resultado = calcularEstadisticas(numeros);
 
-                .reduce((a, b) -> a > b ? a : b);
-
-        maximo.ifPresent(m -> System.out.println("El numero mayor es: " + m));
-
+        System.out.println("Maximo: " + resultado.getMaximo());
+        System.out.println("Minimo: " + resultado.getMinimo());
+        System.out.println("Cantidad: " + resultado.getCantidad());
+        System.out.println("El máximo es multiplo de 2?: " + resultado.isMaximoEsMultiploDeDos());
+        System.out.println("El máximo es divisor de 2?: " + resultado.isMaximoEsDivisorDeDos());
     }
 
+    public static ResultadoEstadistico calcularEstadisticas(List<Integer> numeros) {
+
+        int maximo = numeros.stream()
+                .reduce((a, b) -> a > b ? a : b)
+                .orElseThrow();
+
+        int minimo = numeros.stream()
+                .reduce((a, b) -> a < b ? a : b)
+                .orElseThrow();
+
+        long cantidad = numeros.stream().count();
+
+        boolean esMultiploDeDos = (maximo % 2 == 0) ? true : false;
+
+        
+        boolean esDivisorDeDos = (2 % maximo == 0) ? true : false;
+
+        return new ResultadoEstadistico(
+                maximo,
+                minimo,
+                cantidad,
+                esMultiploDeDos,
+                esDivisorDeDos
+        );
+    }
 }
- 
+
+
+class ResultadoEstadistico {
+
+    private final int maximo;
+    private final int minimo;
+    private final long cantidad;
+    private final boolean maximoEsMultiploDeDos;
+    private final boolean maximoEsDivisorDeDos;
+
+    public ResultadoEstadistico(int maximo, int minimo, long cantidad,
+                                boolean maximoEsMultiploDeDos,
+                                boolean maximoEsDivisorDeDos) {
+        this.maximo = maximo;
+        this.minimo = minimo;
+        this.cantidad = cantidad;
+        this.maximoEsMultiploDeDos = maximoEsMultiploDeDos;
+        this.maximoEsDivisorDeDos = maximoEsDivisorDeDos;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public int getMinimo() {
+        return minimo;
+    }
+
+    public long getCantidad() {
+        return cantidad;
+    }
+
+    public boolean isMaximoEsMultiploDeDos() {
+        return maximoEsMultiploDeDos;
+    }
+
+    public boolean isMaximoEsDivisorDeDos() {
+        return maximoEsDivisorDeDos;
+    }
+}
