b14cdae831aa7c420e51217baf517ec7a93d0dd8
BRAYAN LOAIZA LEAL <alvarezdairo9@gmail.com>
2026-01-27 17:25:12 -0500

    verificación de si el número mayor es multiplo de 2

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,23 +1,68 @@
 import java.util.List;
-import java.util.Optional;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        List<Integer> numeros = List.of(8, 3, 15, 2, 9, 4);
+        List<Integer> numeros = List.of(4, 9, 2, 15, 7);
+
+        ResultadoEstadistico resultado = calcularEstadisticas(numeros);
+
+        System.out.println("Maximo: " + resultado.getMaximo());
+        System.out.println("Minimo: " + resultado.getMinimo());
+        System.out.println("Cantidad: " + resultado.getCantidad());
+        System.out.println("El máximo es multiplo de 2?: " + resultado.isMaximoEsMultiploDeDos());
+    }
 
 
-        Optional<Integer> minimo = numeros.stream()
-                .reduce((a, b) -> a < b ? a : b);
+    public static ResultadoEstadistico calcularEstadisticas(List<Integer> numeros) {
 
+        int maximo = numeros.stream()
+                .reduce((a, b) -> a > b ? a : b)
+                .orElseThrow();
+
+        int minimo = numeros.stream()
+                .reduce((a, b) -> a < b ? a : b)
+                .orElseThrow();
 
         long cantidad = numeros.stream().count();
 
-        minimo.ifPresent(m ->
-                System.out.println("Numero más pequeño: " + m)
-        );
 
-        System.out.println("Cantidad de datos ingresados: " + cantidad);
+        boolean esMultiploDeDos = (maximo % 2 == 0) ? true : false;
+
+        return new ResultadoEstadistico(maximo, minimo, cantidad, esMultiploDeDos);
+    }
+}
+
+
+class ResultadoEstadistico {
+
+    private final int maximo;
+    private final int minimo;
+    private final long cantidad;
+    private final boolean maximoEsMultiploDeDos;
+
+    public ResultadoEstadistico(int maximo, int minimo, long cantidad, boolean maximoEsMultiploDeDos) {
+        this.maximo = maximo;
+        this.minimo = minimo;
+        this.cantidad = cantidad;
+        this.maximoEsMultiploDeDos = maximoEsMultiploDeDos;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public int getMinimo() {
+        return minimo;
+    }
+
+    public long getCantidad() {
+        return cantidad;
+    }
+
+    public boolean isMaximoEsMultiploDeDos() {
+        return maximoEsMultiploDeDos;
     }
 }
 
