39248f10b5dbceef3fb5e9e1999db8142dc58ef3
BRAYAN LOAIZA LEAL <alvarezdairo9@gmail.com>
2026-01-27 16:48:49 -0500

    cracion de la funcion para calcular el numero mas pequeño y la candtidad de datos de una lista

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,1 +1,23 @@
- 
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> numeros = List.of(8, 3, 15, 2, 9, 4);
+
+
+        Optional<Integer> minimo = numeros.stream()
+                .reduce((a, b) -> a < b ? a : b);
+
+
+        long cantidad = numeros.stream().count();
+
+        minimo.ifPresent(m ->
+                System.out.println("Numero más pequeño: " + m)
+        );
+
+        System.out.println("Cantidad de datos ingresados: " + cantidad);
+    }
+}
+
