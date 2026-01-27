c1d88f83e9eef4903e720b35386c2ab192799296
BRAYAN LOAIZA LEAL <alvarezdairo9@gmail.com>
2026-01-27 17:12:27 -0500

    unificacion de las dos funcione reto2

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -1,28 +1,56 @@
 import java.util.List;
-import java.util.Optional;
 
 public class Main {
+
     public static void main(String[] args) {
-        //Funcion Max
-        List<Integer> numeros = List.of(8, 3, 15, 2, 9, 4);
 
-        Optional<Integer> minimo = numeros.stream()
-                .reduce((a, b) -> a < b ? a : b);
-        long cantidad = numeros.stream().count();
-        minimo.ifPresent(m ->
-                System.out.println("Numero más pequeño: " + m)
-        );
-        System.out.println("Cantidad de datos ingresados: " + cantidad);
-
-        //Funcion Min y contar
         List<Integer> numeros = List.of(4, 9, 2, 15, 7);
 
-        Optional<Integer> maximo = numeros.stream()
+        ResultadoEstadistico resultado = calcularEstadisticas(numeros);
 
-                .reduce((a, b) -> a > b ? a : b);
+        System.out.println("Máximo: " + resultado.getMaximo());
+        System.out.println("Mínimo: " + resultado.getMinimo());
+        System.out.println("Cantidad: " + resultado.getCantidad());
+    }
 
-        maximo.ifPresent(m -> System.out.println("El numero mayor es: " + m));
+    // FUNCIÓN ÚNICA (misma para ambos carriles)
+    public static ResultadoEstadistico calcularEstadisticas(List<Integer> numeros) {
+
+        int maximo = numeros.stream()
+                .reduce((a, b) -> a > b ? a : b)
+                .orElseThrow();
+
+        int minimo = numeros.stream()
+                .reduce((a, b) -> a < b ? a : b)
+                .orElseThrow();
+
+        long cantidad = numeros.stream().count();
+
+        return new ResultadoEstadistico(maximo, minimo, cantidad);
     }
 }
 
+class ResultadoEstadistico {
 
+    private final int maximo;
+    private final int minimo;
+    private final long cantidad;
+
+    public ResultadoEstadistico(int maximo, int minimo, long cantidad) {
+        this.maximo = maximo;
+        this.minimo = minimo;
+        this.cantidad = cantidad;
+    }
+
+    public int getMaximo() {
+        return maximo;
+    }
+
+    public int getMinimo() {
+        return minimo;
+    }
+
+    public long getCantidad() {
+        return cantidad;
+    }
+}
