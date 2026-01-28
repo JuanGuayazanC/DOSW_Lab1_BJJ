3e294eb7abe90fca24263d67b98fe22db88fe733
Cruz-Juan-r <juanestebancruzrico@gmail.com>
2026-01-28 11:05:47 -0500

    Update Reto3.java
    solucion error reto 3

--- Laboratorio1/reto3/Reto3.java
+++ Laboratorio1/reto3/Reto3.java
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> b62d6950fe76173bf2e121e3656e73a33b3821f1
 import java.util.stream.IntStream;
 
 public class Reto3 {
@@ -22,26 +17,4 @@ public class Reto3 {
         StringBuffer men = new StringBuffer(cadena);
         return men.reverse().toString();
     }
-<<<<<<< HEAD
-} 
->>>>>>> ba1c2e7e6d820f535002b0a714c8c5df87fb6479
-=======
-public class Reto3 {
-
-    public static String mensaje(String cadena) {
-        StringBuffer men = new StringBuffer(mensaje);
-        return men.reverse().toString();
-    }
-    public static void main(String[] args) {
-
-        String mensaje = "Hola mundo";
-        String resultado = EcoMisterioso.eco(mensaje);
-
-        System.out.println(resultado);
-    }
 }
-
->>>>>>> feature/reto_numero_4
-=======
-}
->>>>>>> b62d6950fe76173bf2e121e3656e73a33b3821f1
