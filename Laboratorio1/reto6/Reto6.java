67d8329d6856f9ff1a25b3cbd2390dbf481efd2f
Juan Sebastián Guayazán Clavijo <juan.guayazan-c@mail.escuelaing.edu.co>
2026-01-27 20:40:59 -0500

    feature: Estudiante B - Fragmento 2 switch-case

--- Laboratorio1/reto6/Reto6.java
+++ Laboratorio1/reto6/Reto6.java
@@ -1,1 +1,21 @@
- 
+public class Reto6 {
+
+    public static void ejecutarComando(String comando) {
+        switch (comando) {
+            case "BROMEAR":
+                System.out.println("¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio…");
+                break;
+            case "GRITAR":
+                System.out.println("¡¡¡ALERTA DE STACK OVERFLOW!!!");
+                break;
+            case "SUSURRAR":
+                System.out.println("Shhh… los bugs están dormidos");
+                break;
+            case "ANALIZAR":
+                System.out.println("Analizando datos… resultado: ¡Eres increíble programando!");
+                break;
+            default:
+                System.out.println("Comando no reconocido.");
+        }
+    }
+} 
