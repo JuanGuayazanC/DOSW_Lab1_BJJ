810c27116cc957183fd6572648ae17ac6edf2049
Juan Esteban Cruz <juanestebancruzrico@gmail.com>
2026-01-27 19:54:57 -0500

    método en su rama que almacene números aleatorios en orden ascendente y elimine los múltiplos de 5.

--- Laboratorio1/reto5/Reto5.java
+++ Laboratorio1/reto5/Reto5.java
@@ -1,1 +1,17 @@
- 
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Reto5 {
+
+    public static Set<Integer> crearTreeSet() {
+        Set<Integer> set = new TreeSet<>(List.of(12, 3, 25, 10, 7, 30, 18, 4));
+        return set.stream()
+                  .filter(n -> n % 5 != 0)
+                  .collect(Collectors.toSet());
+    }
+    
+    public static void main(String[] args) {
+        Set<Integer> resultado = crearTreeSet();
+        System.out.println("Números que no son múltiplos de 5: " + resultado);
+    }
+} 
