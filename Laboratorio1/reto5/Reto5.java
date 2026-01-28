727cecd249abae700b209ee83ac437bce3a93a9b
BRAYAN LOAIZA LEAL <alvarezdairo9@gmail.com>
2026-01-27 19:52:10 -0500

    creacion de metodo que elimina multiplos de tres

--- Laboratorio1/reto5/Reto5.java
+++ Laboratorio1/reto5/Reto5.java
@@ -1,1 +1,18 @@
- 
+package reto5;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Reto5 {
+
+    public static Set<Integer> crearHashSet() {
+        Set<Integer> set = new HashSet<>(List.of(4, 9, 15, 7, 18, 21, 10, 5));
+        return set.stream()
+                .filter(n -> n % 3 != 0)
+                .collect(Collectors.toSet());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(crearHashSet());
+    }
+}
